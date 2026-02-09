472f06fece7d4ad20c837be8a0cfddd5956a13ca
huzaifa-006 <huzaifa.shafiq@icloud.com>
2026-02-09 19:51:30 +0500

    Updated settings.py

--- prescripto/settings.py
+++ prescripto/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-prescripto-2026-very-secure')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '.pythonanywhere.com']
 CSRF_TRUSTED_ORIGINS = [
@@ -96,3 +96,7 @@ except ImportError:
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
